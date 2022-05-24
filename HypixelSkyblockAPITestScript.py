d0e4b7ab2879eb9edc37fce1f2cea69826d9237b
JustinXWT <teohjustin@hotmail.com>
2022-05-25 01:48:12 +0800

    Update HypixelSkyblockAPITestScript.py

--- HypixelSkyblockAPITestScript.py
+++ HypixelSkyblockAPITestScript.py
@@ -5,9 +5,18 @@ import requests
 import json
 from pprint import pprint
 
+profileDict = {}
+
+#Uses the requests library to perform the GET function on specified URL and provides data in json format
 def getInfo(call):
     r = requests.get(call)
     return r.json()
+
+#Created a dictionary storing profile nicknames and IDs
+def createProfileDict():
+    profileData = getInfo(getProfileListLink)
+    for profileID in profileData["profiles"]:
+            profileDict[profileID['cute_name']]=profileID['profile_id']
 
 #Default parameters
 name="Seyr"
@@ -16,37 +25,43 @@ webLink="https://api.hypixel.net/key"
 uuid="8800f769efd84095abfc03eaad0d5291"
 
 #General Hypixel Links
-statusCheckLink = f"https://api.hypixel.net/key?key={apiKey}"
-generalInfoLink = f"https://api.hypixel.net/player?key={apiKey}&name={name}"
-friendsListLink = f"https://api.hypixel.net/friends?key={apiKey}&uuid={uuid}"
+# statusCheckLink = f"https://api.hypixel.net/key?key={apiKey}"
+# generalInfoLink = f"https://api.hypixel.net/player?key={apiKey}&name={name}"
+# friendsListLink = f"https://api.hypixel.net/friends?key={apiKey}&uuid={uuid}"
 
 #Hypixel Skyblock links
 skyblockProfileLink = f"https://api.hypixel.net/skyblock/profile?key={apiKey}&uuid={uuid}"
-skyblockListProfilesLink = f"https://api.hypixel.net/skyblock/profiles?key={apiKey}&uuid={uuid}"
+# skyblockListProfilesLink = f"https://api.hypixel.net/skyblock/profiles?key={apiKey}&uuid={uuid}"
 
 
 minecraftUserName = input("What is your username?\n")
 getMinecraftUUID = f"https://api.mojang.com/users/profiles/minecraft/{minecraftUserName}"
 minecraftUUID = getInfo(getMinecraftUUID)
 print(minecraftUUID["id"])
-testLink = f"https://api.hypixel.net/skyblock/profiles?key={apiKey}&uuid={minecraftUUID['id']}"
+getProfileListLink = f"https://api.hypixel.net/skyblock/profiles?key={apiKey}&uuid={minecraftUUID['id']}"
+createProfileDict()
 
-##Switch statement shennanigans
+
 print("""
 1: Check list of profiles in Skyblock
 2: Check specific Skyblock profile
 """)
 response = input("What would you like to do?\n")
+
+##If else for now possibility of swapping to a switch statement implementation later
 if response == "1":
-    #Created a dictionary storing profile nicknames and IDs
-    profileDict = {}
-    profileData = getInfo(testLink)
-    for profileID in profileData["profiles"]:
-            profileDict[profileID['cute_name']]=profileID['profile_id']
     print("Here are your profile names and profile IDs")
     print(profileDict)
 elif response == "2":
-    print("Function in progress")
+    print(profileDict)
+    specifiedProfile = input("Which profile would you like the information for?\n")
+    if specifiedProfile in profileDict:
+        specificProfileLink = f"https://api.hypixel.net/skyblock/profiles?key={apiKey}&uuid={profileDict[specifiedProfile]}"
+        print(specificProfileLink)
+        specificProfileInformation = getInfo(specificProfileLink)
+        pprint(specificProfileInformation)
+    else:
+        print("Profile does not exist")
 else:
     print("Invalid Input")
 
