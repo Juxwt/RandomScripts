e297df0817fc8fd933bc40c8344c7c2516b8d6ce
JustinXWT <teohjustin@hotmail.com>
2022-05-28 03:46:48 +0800

    Update

--- HypixelSkyblockAPITestScript.py
+++ HypixelSkyblockAPITestScript.py
@@ -16,18 +16,6 @@ def createProfileDict():
     for profileID in profileData["profiles"]:
             profileDict[profileID['cute_name']]=profileID['profile_id']
 
-# def findSpecificProfile(obj):
-#     specificProfileLink = f"https://api.hypixel.net/skyblock/profiles?key={apiKey}&uuid={profileDict[obj]}"
-#     print(specificProfileLink)
-#     specificProfileInformation = getInfo(specificProfileLink)
-#     pprint(specificProfileInformation)
-
-# def saveFileFunc(profileName, accountName):
-#     with open(profileName + accountName +"Skyblock.json", "w") as outfile:
-#             specificProfileLink = f"https://api.hypixel.net/skyblock/profiles?key={apiKey}&uuid={profileDict[profileName]}"
-#             specificProfileInformation = getInfo(specificProfileLink)
-#             profileDataSerialised = json.dumps(specificProfileInformation, indent=4)
-#             outfile.write(profileDataSerialised)
 
 ##Make the 2 separate functions above into a single class therefore reducing the need to rerun grabbing of data twice
 class number2():
@@ -47,17 +35,8 @@ class number2():
 
 #Default parameters
 name="Seyr"
-apiKey="31e6d0f4-ff94-4e27-a297-3c376e535ec0"
+apiKey="de0be782-84ab-4df1-95b6-12ef88a2dd9f"
 uuid="8800f769efd84095abfc03eaad0d5291"
-
-#General Hypixel Links
-# statusCheckLink = f"https://api.hypixel.net/key?key={apiKey}"
-# generalInfoLink = f"https://api.hypixel.net/player?key={apiKey}&name={name}"
-# friendsListLink = f"https://api.hypixel.net/friends?key={apiKey}&uuid={uuid}"
-
-#Hypixel Skyblock links
-# skyblockProfileLink = f"https://api.hypixel.net/skyblock/profile?key={apiKey}&uuid={uuid}"
-# skyblockListProfilesLink = f"https://api.hypixel.net/skyblock/profiles?key={apiKey}&uuid={uuid}"
 
 ##Uses Mojang API to grab provided Minecraft account's UUID
 minecraftUserName = input("What is your username?\n")
@@ -85,17 +64,11 @@ elif response == "2":
     print(profileDict)
     specifiedProfile = input("Which profile would you like the information for?\n")
     if specifiedProfile in profileDict:
-        # specificProfileLink = f"https://api.hypixel.net/skyblock/profiles?key={apiKey}&uuid={profileDict[specifiedProfile]}"
-        # print(specificProfileLink)
-        # specificProfileInformation = getInfo(specificProfileLink)
-        # pprint(specificProfileInformation)
-        # findSpecificProfile(specifiedProfile)
         testingClass = number2()
         testingClass.findSpecificProfile(specifiedProfile)
         saveFile = input("Would you like to save this information into a file?  Y or N?\n")
         if saveFile == "Y":
             print(specifiedProfile + "\n" + minecraftUUID["name"])
-            # saveFileFunc(specifiedProfile, minecraftUUID["name"])
             testingClass.saveFileFunc(specifiedProfile, minecraftUUID["name"])
         else:
             print("File was not created")
@@ -126,3 +99,16 @@ with open("listProfiles.json") as json_file:
         # print(profileID['cute_name'])
         profileDict[profileID['cute_name']]=profileID['profile_id']
         
+
+# def findSpecificProfile(obj):
+#     specificProfileLink = f"https://api.hypixel.net/skyblock/profiles?key={apiKey}&uuid={profileDict[obj]}"
+#     print(specificProfileLink)
+#     specificProfileInformation = getInfo(specificProfileLink)
+#     pprint(specificProfileInformation)
+
+# def saveFileFunc(profileName, accountName):
+#     with open(profileName + accountName +"Skyblock.json", "w") as outfile:
+#             specificProfileLink = f"https://api.hypixel.net/skyblock/profiles?key={apiKey}&uuid={profileDict[profileName]}"
+#             specificProfileInformation = getInfo(specificProfileLink)
+#             profileDataSerialised = json.dumps(specificProfileInformation, indent=4)
+#             outfile.write(profileDataSerialised)
---
+++ RandomDecisionMakingPandasJikan.py
@@ -0,0 +1,152 @@
+from cgi import test
+import requests
+import json
+import pandas as pd
+from pprint import pprint as pp
+
+baseUrl = "https://api.jikan.moe/v4"
+# print(getAnimeById)
+getAnimeSearch = f"https://api.jikan.moe/v4/anime"
+getAnimeGenres = f"https://api.jikan.moe/v4/genres/anime"
+
+def convertToPandas(arg):
+        genreListDataframe = pd.DataFrame.from_dict(arg)
+        
+
+#! Probably have to move everything into a class and be able to call genres from there?
+def simpleRequest(url):
+    r = requests.get(url)
+    print(f"Status code is: {r.status_code}")
+    # pp(r.json()['data'])
+    return r.json()
+
+#? Trying to make it return the json value outside of the function
+genreList = {}
+
+class getGenre():
+    def __init__(self):
+        self.genreList = None
+
+    def getGenreList(self):
+        url = baseUrl + "/genres/anime"
+        self.genreList = simpleRequest(url)
+        # pp(self.genreList)
+        return self.genreList['data']
+    
+    
+        
+
+genreData = getGenre()
+genreList = genreData.getGenreList()
+genreList = pd.DataFrame(genreList)
+
+# ['data']
+pp(genreList)
+pp(type(genreList))
+    
+
+class animeSearch():
+    
+    def __init__(self):
+        self.apiUrl = "https://api.jikan.moe/v4/"
+
+    def getGenres(self):
+        url = self.apiUrl + ""
+
+
+#?Creates dataframe that contians dataset information
+# animeListPanda = pd.DataFrame({
+#      "Anime Name":[
+#          "Bakemonogatari",
+#          "Naruto",
+#          "Bleach",
+#          "Mushoku Tensei",
+#          "Mato Seihei no Slave",
+#          "Sono Bisque Doll wa Koi wo Suru",
+#          "Kizumonogatari I",
+#          "Kizumonogatari II",
+#          "AMOGUS",
+#          "SAMPLE TEXT"
+#      ],
+#      "Genre":[
+#          "Vampire",
+#          "Shounen",
+#          "Shounen",
+#          "Isekai",
+#          "Shounen",
+#          "Seinen",
+#          "Vampire",
+#          "Vampire",
+#          "AMOGUS",
+#          "SAMPLE TEXT"
+#      ],
+#      "Type":[
+#          "TV",
+#          "TV",
+#          "TV",
+#          "TV",
+#          "TV",
+#          "TV",
+#          "Movie",
+#          "Movie",
+#          "AMOGUS",
+#          "SAMPLE TEXT"
+#      ],
+#      "Rating":[
+#          "1",
+#          "2",
+#          "3",
+#          "4",
+#          "5",
+#          "6",
+#          "7",
+#          "7",
+#          "8",
+#          "9",
+#      ],
+#  }
+#  )
+# print(animeListPanda)
+# print(animeListPanda[["Genre","Anime Name"]])
+
+
+# #?Puts all genres into a list and removes duplicate genres
+# all_genre = list(dict.fromkeys(animeListPanda["Genre"].values.tolist()))
+
+# print("These are the series types:")
+
+# for i in range(len(all_genre)):
+#     #?Displays all genres line by line
+#     print(all_genre[i])
+
+# user_answers = {}
+
+# #?Accepts user input to be used for filtering
+# user_answers["animeType"] = input("What kind of Anime do you want to watch?\n")
+# user_answers["seriesType"] = input("Do you want to watch a TV show or Movie?\n")
+
+# print(user_answers)
+
+# #! Filter the dataframe based on provided input
+# filteredAnimeList = animeListPanda["Genre"] == user_answers["animeType"] 
+# filteredAnimeList = animeListPanda[filteredAnimeList]
+# filteredAnimeListType = filteredAnimeList["Type"] == user_answers["seriesType"] 
+# filteredAnimeList = filteredAnimeList[filteredAnimeListType]
+
+# # print(filteredAnimeList)
+# # print(filteredAnimeList.iloc[0].values[0])
+
+# if len(filteredAnimeList) > 0:
+#     rngPick = filteredAnimeList.sample()
+#     rngPick = rngPick.values.tolist()
+#     # print(rngPick)
+#     # print(f"""Recommendation Anime:{rngPick.to_string(index=False)} """)
+#     print(f"""
+#     Recommendation
+#     Anime: {rngPick[0][0]}
+#     Rating: {rngPick[0][3]}
+#     Genre: {rngPick[0][1]}
+#     Type: {rngPick[0][2]}
+#     """)
+# else:
+#     print(""" Unable to recommend a series.\n Either one does not exist or the database does not contain one.""")
